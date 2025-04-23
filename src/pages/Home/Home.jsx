607cdd18d2511c0ba94c2262b6ce72657fc7b85c
zourte2486 <zourte2486@gmail.com>
2025-04-23 02:49:54 +0200

    Refactor Navbar styles and enhance Home page layout with crypto table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,28 @@ function Home() {
   return (
     <div className="home">
       <div className="hero">
-        <h1> A9wed <br />
-        Crypto Marketplace </h1>
-        <p>Welcome to the world's A9wed cryptocurrency marketplace. <br />
-        Sign up to explore more about cryptos.</p>
+        <h1>
+          {" "}
+          A9wed <br />
+          Crypto Marketplace{" "}
+        </h1>
+        <p>
+          Welcome to the world's A9wed cryptocurrency marketplace. <br />
+          Sign up to explore more about cryptos.
+        </p>
         <form>
           <input type="text" placeholder="Search crypto.." />
           <button type="submit">Search</button>
         </form>
+        <div className="crypto-table">
+          <div className="table-layout">
+            <p>#</p>
+            <p>Coins</p>
+            <p>Price</p>
+            <p style={{ textAlign: "center" }}>24H Change</p>
+            <p className="market-cap">Market Cap</p>
+          </div>
+        </div>
       </div>
     </div>
   );
