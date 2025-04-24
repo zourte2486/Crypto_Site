bb65077d78ba515298da0120dfa702c795a66458
zourte2486 <zourte2486@gmail.com>
2025-04-24 02:11:57 +0200

    Enhance CoinContextProvider

--- src/Context/CoinContext.jsx
+++ src/Context/CoinContext.jsx
@@ -1,42 +1,45 @@
-import {  createContext, useEffect } from "react";
-import { CoinContext } from './CoinContext';
+import { createContext, useEffect,useState } from "react";
 
+export const CoinContext = createContext();
 
-export const CoinContext = createContext(); 
+const CoinContextProvider = (props) => {
+  const [allCoins, setAllCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "USD",
+    symbol: "$",
+  });
+  const fetchAllCoins = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-Rwux4WAqZ1UJghbvW9iTG9Sd	",
+      },
+    };
 
-const CoinContextProvider = ( props ) => {
-    const [allCoins, setAllCoin] = useState([]);
-    const [currency, setCurrency] = useState({
-        name: "USD",
-        symbol: "$"
-    }
-    );
-    const fetchAllCoins = async () => {
-        const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              'x-cg-demo-api-key': 'CG-Rwux4WAqZ1UJghbvW9iTG9Sd	'
-            }
-          };
-          
-          fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
-            .then(res => res.json())
-            .then(res => setAllCoin(res))
-            .catch(err => console.error(err));
-    }
-        useEffect(() => {
-            fetchAllCoins();
-        },[])
-    const contextvalue = {
-
-    }
-
-    return (
-        <CoinContext.Provider value={contextValue}>
-            {props.children}
-        </CoinContext.Provider>
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+      options
     )
-}
+      .then((res) => res.json())
+      .then((res) => setAllCoin(res))
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    fetchAllCoins();
+  }, [currency]);
+
+  const contextvalue = {
+    allCoins,
+    currency,
+    setCurrency,
+  };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
 
 export default CoinContextProvider;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { useContext } from "react";
+import { CoinContext } from "../../Context/CoinContext";
 
 const Navbar = () => {
+
+  const { setCurrency } = useContext(CoinContext);
+  const handleCurrencyChange = (event) => {
+    switch(event.target.value){
+      case "usd":
+        setCurrency({
+          name: "USD",
+          symbol: "$"
+        });
+        break;
+      case "eur":
+        setCurrency({
+          name: "EUR",
+          symbol: "€"
+        });
+        break;
+      case "mad":
+        setCurrency({
+          name: "MAD",
+          symbol: "MAD"
+        });
+        break;
+      default:
+        setCurrency({
+          name: "USD",
+          symbol: "$"
+        });
+    }
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="logo" />
@@ -13,7 +45,7 @@ const Navbar = () => {
         <li className="hover-underline">Blog</li>
       </ul>
       <div className="nav-right">
-      <select>
+      <select onChange={handleCurrencyChange}>
           <option value="usd">Usd</option>
           <option value="eur">Eur</option>
           <option value="mad">Mad</option>
