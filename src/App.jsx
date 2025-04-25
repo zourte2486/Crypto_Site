94e928550c2a883d6b1326d206187cdbd777b922
zourte2486 <zourte2486@gmail.com>
2025-04-25 16:58:27 +0200

    Add Footer component with social media links and styles; update Navbar responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Coin from "./pages/Coin/Coin.jsx";
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/coin/:coinId" element={<Coin />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+      <div className="footer-content">
+        <p>© 2025 NovaChain. All rights reserved.</p>
+        <p>Follow us on:</p>
+        <ul className="social-media-links">
+          <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+          <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+          <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        </ul>
+      </div>
   )
 }
 
