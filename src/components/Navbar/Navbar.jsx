8109f12721b017617d2a9f117c588f9f2bcef1c3
zourte2486 <zourte2486@gmail.com>
2025-04-27 20:31:01 +0200

    fix: Correct CSS file import case for Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./Navbar.css";
+import "./navbar.css";
 import logo from "../../assets/logo.png";
 import { CoinContext } from "../../Context/CoinContext";
 import { useNavigate } from "react-router-dom";
